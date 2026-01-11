1252bf5a2138a834d45c162958ef7d5c6662cf4b
Daniel Kifer <dkifer@cse.psu.edu>
2026-01-11 15:30:44 -0500

    .

---
+++ githw/githw.py
@@ -0,0 +1,18 @@
+
+def main():
+    print("")
+    print("###################################")
+    print("Me haz not finished git hw :-(")
+    print("###################################")
+    print("      ___                          ")
+    print("  _.-|   |          |\__/,|   (`\  ")
+    print(" {   |   |          |o o  |__ _) ) ")
+    print('  "-.|___|        _.( T   )  `  /  ')
+    print("   .--'-`-.     _((_ `^--' /_<  \  ")
+    print(" .+|______|__.-||__)`-'(((/  (((/  ")
+    print("https://user.xmission.com/~emailbox/ascii_cats.htm")
+    print("")
+
+
+if __name__ == "__main__":
+    main()
